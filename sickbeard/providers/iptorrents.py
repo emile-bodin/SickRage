ed25d334592e74e4e941c88e7a6b3f08ee2d7344
Emile Bodin <emile@hostinglab.nl>
2015-04-19 00:10:38 +0200

    Update iptorrents.py
    Iptorrents Changed their urls, the original scripts gave me errors. After the changes i get the correct search results again

--- sickbeard/providers/iptorrents.py
+++ sickbeard/providers/iptorrents.py
@@ -57,9 +57,9 @@ class IPTorrentsProvider(generic.TorrentProvider):
 
         self.cache = IPTorrentsCache(self)
 
-        self.urls = {'base_url': 'https://iptorrents.eu',
-                'login': 'https://iptorrents.eu/torrents/',
-                'search': 'https://iptorrents.eu/torrents/?%s%s&q=%s&qf=ti',
+        self.urls = {'base_url': 'https://iptorrents.com',
+                'login': 'https://iptorrents.com/t',
+                'search': 'https://iptorrents.com/t?%s%s&q=%s&qf=ti',
         }
 
         self.url = self.urls['base_url']
